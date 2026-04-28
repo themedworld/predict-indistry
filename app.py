eae9272c6fc9cff411d6fb624742e35d842733bc
themedworld <99541159+themedworld@users.noreply.github.com>
2026-04-28 13:05:11 +0100

    final commit

--- app.py
+++ app.py
@@ -1,40 +1,35 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from gradio_client import Client
 
-# =========================
-# APP
-# =========================
 app = FastAPI(title="Industry Prediction API")
 
-# =========================
-# GRADIO CLIENT
-# =========================
+# Autoriser ton frontend Next.js
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 client = Client("Traii/predict-industry-api")
 
-# =========================
-# REQUEST MODEL
-# =========================
 class ServiceRequest(BaseModel):
     service: str
 
-# =========================
-# API ENDPOINT
-# =========================
 @app.post("/predict-service")
 def predict_service(data: ServiceRequest):
-
     try:
         result = client.predict(
             service=data.service,
             api_name="/predict_service"
         )
-
         return {
             "success": True,
             "input": data.service,
             "prediction": result
         }
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
